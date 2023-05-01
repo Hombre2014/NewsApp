37197c8bd667cdf217b841ac36def466208f97b0
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2023-05-01 11:34:13 +0200

    Adding redux

--- App.js
+++ App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import RootNavigation from './src/navigation';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+import {persistStore} from 'redux-persist';
+import reduxStore from './src/redux';
+
+export const reduxPersistStore = persistStore(reduxStore);
 
 const App = () => {
-  return <RootNavigation />;
+  return (
+    <Provider store={reduxStore}>
+      <PersistGate persistor={reduxPersistStore}>
+        <RootNavigation />
+      </PersistGate>
+    </Provider>
+  );
 };
 
 export default App;
---
+++ src/redux/middleware/ApiCalls.js
@@ -0,0 +1,28 @@
+const promiseMiddleware = () => {
+  return next => action => {
+    const {promise, type, ...rest} = action;
+
+    if (!promise) {
+      return next(action);
+    }
+
+    const SUCCESS = type + '_SUCCESS';
+    const REQUEST = type + 'REQUEST';
+    const FAILURE = type + '_FAILURE';
+
+    next({...rest, type: REQUEST});
+
+    return promise
+      .then(response => {
+        next({...rest, response: response, type: SUCCESS});
+        return true;
+      })
+      .catch(error => {
+        next({...rest, error, type: FAILURE});
+        console.log(error);
+        return false;
+      });
+  };
+};
+
+export default promiseMiddleware;
